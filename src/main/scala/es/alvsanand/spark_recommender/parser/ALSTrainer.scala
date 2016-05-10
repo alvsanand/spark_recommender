e8afde2e4e385ec65c0690f34b97068dbb858425
asantos <asantos@stratio.com>
2016-05-10 16:28:35 +0200

    Refactorized ALS code

--- src/main/scala/es/alvsanand/spark_recommender/parser/ALSTrainer.scala
+++ src/main/scala/es/alvsanand/spark_recommender/parser/ALSTrainer.scala
@@ -32,7 +32,7 @@ object ALSTrainer {
 
   def productRecommendationStructure(): List[StructField] = {
     List(
-      StructField("id", StringType),
+      StructField("pid", StringType),
       StructField("r", DoubleType)
     )
   }
@@ -40,14 +40,14 @@ object ALSTrainer {
   def userRecsStructure(): List[StructField] = {
     List(
       StructField("userId", StringType),
-      StructField("recs", StructType(productRecommendationStructure))
+      StructField("recs", ArrayType(StructType(productRecommendationStructure)))
     )
   }
 
   def productRecsStructure(): List[StructField] = {
     List(
       StructField("productId", StringType),
-      StructField("recs", StructType(productRecommendationStructure))
+      StructField("recs", ArrayType(StructType(productRecommendationStructure)))
     )
   }
 
@@ -62,9 +62,9 @@ object ALSTrainer {
 
     val ratingsDF = sqlContext.sql("select userId, productId, overall from reviews where userId is not null and productId is not null and overall is not null").cache
 
-    val ratingsRDD = ratingsDF.map(x => Rating(x.getAs[String]("userId").hashCode, x.getAs[String]("productId").hashCode, x.getAs[Double]("overall").toFloat))
-    val usersRDD: RDD[(Int, String)] = ratingsDF.map(x => x.getAs[String]("userId")).map(x => (x.hashCode, x))
-    val productsRDD: RDD[(Int, String)] = ratingsDF.map(x => x.getAs[String]("productId")).map(x => (x.hashCode, x))
+    val ratingsRDD = ratingsDF.map { row => Rating(row.getAs[String]("userId").hashCode, row.getAs[String]("productId").hashCode, row.getAs[Double]("overall").toFloat) }
+    val usersRDD: RDD[(Int, String)] = ratingsDF.map( row => row.getAs[String]("userId") ).map( userId => (userId.hashCode, userId) )
+    val productsRDD: RDD[(Int, String)] = ratingsDF.map( row => row.getAs[String]("productId") ).map( productId => (productId.hashCode, productId) )
     val productsMap: Broadcast[Map[Int, String]] = sc.broadcast(productsRDD.collect().toMap)
 
     val rank = 10
@@ -72,7 +72,7 @@ object ALSTrainer {
     val model = ALS.train(ratingsRDD, rank, numIterations, 0.01)
 
     calculateUserRecs(maxRecs, model, usersRDD, productsMap)
-    calculateProductRecs(maxRecs, model, productsRDD)
+    calculateProductRecs(maxRecs, model, productsMap.value)
   }
 
   private def calculateUserRecs(maxRecs: Int, model: MatrixFactorizationModel, usersRDD: RDD[(Int, String)], productsMap: Broadcast[Map[Int, String]])(implicit _conf: SparkConf, mongoConf: MongoConfig): Unit = {
@@ -81,7 +81,7 @@ object ALSTrainer {
 
     val userRecs: RDD[Row] = model.recommendProductsForUsers(maxRecs)
       .join(usersRDD)
-      .map { x => Row(x._2._2, x._2._1.map { x => Row(productsMap.value.get(x.product), x.rating) }.toList) }
+      .map { case (userIdInt, (ratings, userId))  => Row(userId, ratings.map { rating => Row(productsMap.value.get(rating.product).get, rating.rating) }.toList) }
 
     val userRecsConfig = MongodbConfigBuilder(Map(Host -> mongoConf.hosts.split(";").toList, Database -> mongoConf.db, Collection -> "userRecs"))
 
@@ -89,34 +89,37 @@ object ALSTrainer {
       .saveToMongodb(userRecsConfig.build)
   }
 
-  private def calculateProductRecs(maxRecs: Int, model: MatrixFactorizationModel, productsRDD: RDD[(Int, String)])(implicit _conf: SparkConf, mongoConf: MongoConfig): Unit = {
+  private def calculateProductRecs(maxRecs: Int, model: MatrixFactorizationModel, productsMap: Map[Int, String])(implicit _conf: SparkConf, mongoConf: MongoConfig): Unit = {
     val sc = SparkContext.getOrCreate(_conf)
     val sqlContext = SQLContext.getOrCreate(sc)
 
-    val productRecs: RDD[Row] = productsRDD.map { x =>
-      val itemFactor = model.productFeatures.lookup(x._1).head
+    val productRecs: RDD[Row] = sc.parallelize(productsMap.toList.map { case (idInt, id) =>
+      val itemFactor = model.productFeatures.lookup(idInt).head
       val itemVector = new DoubleMatrix(itemFactor)
-      cosineSimilarity(itemVector, itemVector)
-      val sims = model.productFeatures.map { case (id, factor) =>
+
+      val sims: RDD[(String, Double)]  = model.productFeatures.map { case (currentIdInt, factor) =>
+        val currentId = productsMap(currentIdInt)
+
         val factorVector = new DoubleMatrix(factor)
         val sim = cosineSimilarity(factorVector, itemVector)
-        (x._2, sim)
+
+        (currentId, sim)
       }
 
-      val recs = sims.top(maxRecs)(Ordering.by[(String, Double), Double] { case (id, similarity) => similarity })
+      val recs = sims.filter{ case (currentId, sim) => currentId != id }
+                     .top(maxRecs)(Ordering.by[(String, Double), Double] { case (currentId, sim) => sim })
+                     .map { case (currentId, sim) => Row(currentId, sim) }.toList
 
-      Row(x._2, recs.map(x => Row(x._1, x._2)).toList)
-    }
+      Row(id, recs)
+    })
 
     val productRecsConfig = MongodbConfigBuilder(Map(Host -> mongoConf.hosts.split(";").toList, Database -> mongoConf.db, Collection -> "productRecs"))
 
     sqlContext.createDataFrame(productRecs, StructType(productRecsStructure))
-      .saveToMongodb(productRecsConfig.build)
+              .saveToMongodb(productRecsConfig.build)
   }
 
   private def cosineSimilarity(vec1: DoubleMatrix, vec2: DoubleMatrix): Double = {
     vec1.dot(vec2) / (vec1.norm2() * vec2.norm2())
   }
-
-  //  private def
 }
--- src/main/scala/es/alvsanand/spark_recommender/parser/ProductParser.scala
+++ src/main/scala/es/alvsanand/spark_recommender/parser/ProductParser.scala
@@ -32,7 +32,7 @@ object ProductParser {
     val productReviewsRDD = products.mapPartitions(mapPartitions).cache()
 
     storeDataInMongo(productReviewsRDD)
-    storeDataInES(productReviewsRDD.map(x => x._1))
+    storeDataInES(productReviewsRDD.map { case ( product, reviews ) => product } )
   }
 
   private def storeDataInMongo(productReviewsRDD: RDD[(Product, Option[List[Review]])])(implicit _conf: SparkConf, mongoConf: MongoConfig): Unit = {
@@ -43,8 +43,8 @@ object ProductParser {
     val sqlContext = SQLContext.getOrCreate(sc)
     import sqlContext.implicits._
 
-    productReviewsRDD.map(x => x._1).toDF().distinct().saveToMongodb(productConfig.build)
-    productReviewsRDD.flatMap(x => x._2.getOrElse(List[Review]())).toDF().distinct().saveToMongodb(reviewsConfig.build)
+    productReviewsRDD.map { case ( product, reviews ) => product } .toDF().distinct().saveToMongodb(productConfig.build)
+    productReviewsRDD.flatMap { case ( product, reviews ) => reviews.getOrElse(List[Review]()) }.toDF().distinct().saveToMongodb(reviewsConfig.build)
   }
 
   private def storeDataInES(productReviewsRDD: RDD[Product])(implicit _conf: SparkConf, esConf: ESConfig): Unit = {
@@ -62,26 +62,26 @@ object ProductParser {
   private def mapPartitions(rows: Iterator[Row]): Iterator[(Product, Option[List[Review]])] = {
     val df = new SimpleDateFormat("MMMM dd, yyyy", Locale.US)
 
-    rows.flatMap { x =>
-      if(x.fieldIndex("ProductInfo") == -1 || x.getAs[Row]("ProductInfo").fieldIndex("ProductID") == -1 || x.getAs[Row]("ProductInfo").getAs[String]("ProductID")==null){
+    rows.flatMap { row =>
+      if(row.fieldIndex("ProductInfo") == -1 || row.getAs[Row]("ProductInfo").fieldIndex("ProductID") == -1 || row.getAs[Row]("ProductInfo").getAs[String]("ProductID")==null){
         None
       }
       else{
-        val productRow = x.getAs[Row]("ProductInfo")
+        val productRow = row.getAs[Row]("ProductInfo")
         val product = new Product(productRow.getAs[String]("ProductID"), productRow.getAs[String]("Name"), productRow.getAs[String]("Price"), productRow.getAs[String]("Features"), productRow.getAs[String]("ImgURL"))
 
-        val reviews = x.fieldIndex("Reviews") match {
+        val reviews = row.fieldIndex("Reviews") match {
           case i if i > -1 =>
-            Option(x(i).asInstanceOf[mutable.WrappedArray[Row]].map { x =>
-              val date: java.sql.Timestamp = x.getAs[String]("Date") match {
+            Option(row(i).asInstanceOf[mutable.WrappedArray[Row]].map { reviewRow =>
+              val date: java.sql.Timestamp = reviewRow.getAs[String]("Date") match {
                 case s: String => new java.sql.Timestamp(df.parse(s).getTime)
                 case null => null
               }
-              val overall: Option[Double] = x.getAs[String]("Overall") match {
+              val overall: Option[Double] = reviewRow.getAs[String]("Overall") match {
                 case "None" => None
                 case s: String => Option(s.toDouble)
               }
-              new Review(x.getAs[String]("ReviewID"), x.getAs[String]("Author"), product.productId, x.getAs[String]("Title"), overall, x.getAs[String]("Content"), date)
+              new Review(reviewRow.getAs[String]("ReviewID"), reviewRow.getAs[String]("Author"), product.productId, reviewRow.getAs[String]("Title"), overall, reviewRow.getAs[String]("Content"), date)
             }.toList)
           case -1 => None
         }
